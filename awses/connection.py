943de23af1a5fe3d8fdfcbfb693ca5e27375c330
scottvdp <scott@elelsee.com>
2015-10-21 12:17:38 -0400

    adding body to request

--- awses/connection.py
+++ awses/connection.py
@@ -1,6 +1,7 @@
 from elasticsearch import Connection
 from signer import ESConnection
 from urlparse import urlparse
+import time
 
 
 class AWSConnection(Connection):
@@ -13,8 +14,20 @@ class AWSConnection(Connection):
 
     def perform_request(self, method, url, params=None,
                         body=None, timeout=None, ignore=()):
+        start = time.time()
         host = urlparse(self.host).netloc.split(':')[0]
         client = ESConnection(region=self.region, host=host)
-        response = client.make_request(method, path=url, params=params)
+
+        if body:
+            response = client.make_request(method, path=url, params=params, data=body)
+        else:
+            response = client.make_request(method, path=url, params=params)
+
+        duration = time.time() - start
         raw_data = response.read().decode('utf-8')
+
+        if not (200 <= response.status < 300) and response.status not in ignore:
+            self.log_request_fail(method, host, body, duration, response.status)
+            self._raise_error(response.status, raw_data)
+
         return response.status, dict(response.getheaders()), raw_data
