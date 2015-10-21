bedf47b60dfb41b3a785a8c251a33f0f348bd507
scottvdp <scott@elelsee.com>
2015-10-21 11:25:09 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='aws-es-connection',
+      version='0.1',
+      description='Python Elasticsearch Client connection class for AWS Elasticsearch Service',
+      author='Scott VanDenPlas',
+      author_email='scott@elelsee.com',
+      url='https://github.com/elelsee/aws-es-connection',
+      packages=find_packages(),
+      py_modules=['awses'],
+      install_requires=['boto', 'elasticsearch'])
